41caa2fd6094aec73ea238a927c1a8d07ecf815d
viktor <svmotric@gmail.com>
2021-06-03 11:56:48 +0100

    updated onset outputs for constrained generation, extra output columns for quicker spotchecks, updated video generation to work with constrained onset dbs

--- video_sequence generator.py
+++ video_sequence generator.py
@@ -8,27 +8,33 @@ import shutil
 clear_img = 'src_vid_images/izgray.png'
 onset_img = 'src_vid_images/iznotonlygray.png'
 db_paths = []
-for _, _, files in os.walk('onset_output', topdown=True):
+for _, _, files in os.walk('c_onset_output', topdown=True):
     # print(files)
     for f in files:
-        file_path = os.path.join('onset_output', f)
+        file_path = os.path.join('c_onset_output', f)
         db_paths.append(file_path)
 
 for part_file in db_paths:
     df = pd.read_csv(part_file, parse_dates=True)
-    part_no = part_file.split('_')[-1].split('.')[0]
+    # part_no = part_file.split('_')[-1].split('.')[0]
+    part_no = part_file.split('_')[-2]
     # print(df.head(5))
     # print(df.columns)
+    # vid_col_labels = [
+    #                 ['hch_beats', 'hch_onsets', 'hch_onsets_abs'],
+    #                 ['hhc_beats', 'hhc_onsets', 'hhc_onsets_abs'],
+    #                 ['nhp_beats', 'nhp_onsets', 'nhp_onsets_abs'],
+    #                 ['nhnp_beats', 'nhnp_onsets', 'nhnp_onsets_abs']]
     vid_col_labels = [
-                    ['hch_beats', 'hch_onsets', 'hch_onsets_abs'],
-                    ['hhc_beats', 'hhc_onsets', 'hhc_onsets_abs'],
-                    ['nhp_beats', 'nhp_onsets', 'nhp_onsets_abs'],
-                    ['nhnp_beats', 'nhnp_onsets', 'nhnp_onsets_abs']]
-    vid_paths = ['vid_out/hch', 'vid_out/hhc', 'vid_out/nhp', 'vid_out/nhnp']
-    skip_array = [0, 1, 2]
+        ['nhp_beats_const', 'nhp_onsets_const', 'nhp_onsets_abs_const'],
+        ['nhnp_beats_const', 'nhnp_onsets_const', 'nhnp_onsets_abs_const']]
+
+    # vid_paths = ['vid_out/hch', 'vid_out/hhc', 'vid_out/nhp', 'vid_out/nhnp']
+    vid_paths = ['vidc_out/nhpC', 'vidc_out/nhnpC']
+    # skip_array = [0, 1, 2]
     for idx, vid_col in enumerate(vid_col_labels):
-        if idx in skip_array:
-            continue
+        # if idx in skip_array:
+        #     continue
         tmp_df = df[vid_col]
         # print(tmp_df.head(5))
         # print(tmp_df.columns)
@@ -67,7 +73,7 @@ for part_file in db_paths:
         c = 0
         for img_path in img_path_array:
             c += 1
-            if c % 2500 == 0:
+            if c % 12500 == 0:
                 print('%d/%d' % (c, len(img_path_array)))
             img = cv2.imread(img_path)
             height, width, layers = img.shape
@@ -75,8 +81,9 @@ for part_file in db_paths:
             out.write(img)
 
         out.release()
-        if idx == 0:
-            new_path = '%s_%s.avi' % (vid_paths[1], part_no)
-            shutil.copy(vid_path, new_path)
+        print('Done with %s (%dth)' % (part_no, idx))
+        # if idx == 0:
+        #     new_path = '%s_%s.avi' % (vid_paths[1], part_no)
+        #     shutil.copy(vid_path, new_path)
         # break
     # break
